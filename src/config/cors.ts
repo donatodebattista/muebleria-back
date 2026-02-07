ad4cba5f399fc7fc342e52c4b008c26dea22f596
donatodebattista <doni.db@hotmail.com>
2026-02-06 22:31:31 -0300

    fix CORS error

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,15 +1,20 @@
 import { CorsOptions } from "cors";
 
 export const corsConfig : CorsOptions = {
-  origin: function (origin, callback){
-    const allowedOrigins = [process.env.FRONT_URL];
-    if (process.argv[2] === '--api'){
-        allowedOrigins.push(undefined)
-    }
+    origin: function (origin, callback){
+        const allowedOrigins = [
+            process.env.FRONT_URL,
+            'https://muebleriadonpepe.netlify.app',
+            'http://localhost:5173'
+        ];
+        if (process.argv[2] === '--api'){
+                allowedOrigins.push(undefined)
+        }
 
-    if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-    } else {
-        callback(new Error('ERROR de CORS: Acceso Denegado'));
+        if (allowedOrigins.includes(origin)) {
+                callback(null, true);
+        } else {
+                callback(new Error('ERROR de CORS: Acceso Denegado'));
+        }
     }
-}}
+}
